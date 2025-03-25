cde4149d386310d04f1c91e7e97030d4006bd0d6
p2002814 <edgar.demeude@etu.univ-lyon1.fr>
2025-03-25 20:03:17 +0100

    added color theme

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,10 +1,10 @@
 import Map from "@/components/layout/map";
+import { Button } from "@components/ui/button";
 
 export default function Home() {
   return (
     <main className="w-full h-[calc(100vh-64px)] overflow-hidden">
       <Map />
-      
     </main>
   );
 }
